df7632ac5c9c654afd2e2c03f69c9d2002597653
Maruf <marufsayfiyev483@gmail.com>
2023-03-10 22:45:37 +0500

    salom

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <Box>
+            
+        </Box>
+    );
+}
+
+export default Navbar;
